57287bf71591fd862a4bfd6dceb6e78d2d0c6dc8
Marko Suominen <suominem@AMCiMAC2.local>
2012-04-02 16:22:05 +0300

    jotain hahmottelua

--- assets/js/gamelibs/tractor.js
+++ assets/js/gamelibs/tractor.js
@@ -15,4 +15,44 @@ var Game = {
         return 'jokin arvo'
 
     }
+
+    // global array of playernames should be elsewere
+    var players = ["arttu", "teemu", "", "",
+                   "marko", "kristian", "", "",
+                   "", "", "", "",
+                   "", "", "", "",
+                   "", "", "", "",
+                   "", "", "", ""];
+
+    for (var i = 0; i < players.length; i+2;) {
+        if (players[i].length > 0 && players[i+1].length > 0) {
+            switch (i) {
+                case 0: // Create farm1 and tractor1
+                    break;
+                case 2; // Create tractor2
+                    break;
+                case 4: // Create farm2 and tractor1
+                    break;
+                case 6; // Create tractor2
+                    break;
+                case 8: // Create farm3 and tractor1
+                    break;
+                case 10; // Create tractor2
+                    break;
+                case 12: // Create farm4 and tractor1
+                    break;
+                case 14; // Create tractor2
+                    break;
+                case 16: // Create farm5 and tractor1
+                    break;
+                case 18; // Create tractor2
+                    break;
+                case 20: // Create farm6 and tractor1
+                    break;
+                case 22; // Create tractor2
+                    break;
+            }
+
+        }
+    }
 }
