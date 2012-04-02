8df3f594833d76a39b998b3abd35dd8cf6df68f3
Teemu <teemu@dyn-156-200.public.tut.fi>
2012-04-02 14:58:11 +0300

    tractor.js

---
+++ assets/js/gamelibs/tractor.js
@@ -0,0 +1,18 @@
+var Game = {
+    jokinmuuttuja: null,
+    jokinFunc: function(timer) {
+        return 'tein jotain';
+    },
+    zeroPad: function(num, count) {
+        var numZeropad = num + '';
+        while (numZeropad.length < count) {
+            numZeropad = "0" + numZeropad;
+        }
+        return numZeropad;
+    },
+    previousPos: function(current, direction) {
+
+        return 'jokin arvo'
+
+    }
+}
