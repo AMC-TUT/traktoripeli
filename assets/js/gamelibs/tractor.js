8179c415e078ad43035e20d78cd442b2621c101b
Teemu <teemu@Teemus-MacBook.local>
2012-04-03 09:03:47 +0300

    typo fixejä

--- assets/js/gamelibs/tractor.js
+++ assets/js/gamelibs/tractor.js
@@ -14,45 +14,51 @@ var Game = {
 
         return 'jokin arvo'
 
-    }
+    },
+    masunFunktio: function() {
 
-    // global array of playernames should be elsewere
-    var players = ["arttu", "teemu", "", "",
-                   "marko", "kristian", "", "",
-                   "", "", "", "",
-                   "", "", "", "",
-                   "", "", "", "",
-                   "", "", "", ""];
+        // global array of playernames should be elsewere
+        var players = ["arttu", "teemu", "", "",
+                       "marko", "kristian", "", "",
+                       "", "", "", "",
+                       "", "", "", "",
+                       "", "", "", "",
+                       "", "", "", ""];
 
-    for (var i = 0; i < players.length; i+2;) {
-        if (players[i].length > 0 && players[i+1].length > 0) {
-            switch (i) {
-                case 0: // Create farm1 and tractor1
-                    break;
-                case 2; // Create tractor2
-                    break;
-                case 4: // Create farm2 and tractor1
-                    break;
-                case 6; // Create tractor2
-                    break;
-                case 8: // Create farm3 and tractor1
-                    break;
-                case 10; // Create tractor2
-                    break;
-                case 12: // Create farm4 and tractor1
-                    break;
-                case 14; // Create tractor2
-                    break;
-                case 16: // Create farm5 and tractor1
-                    break;
-                case 18; // Create tractor2
-                    break;
-                case 20: // Create farm6 and tractor1
-                    break;
-                case 22; // Create tractor2
-                    break;
+        // http://documentcloud.github.com/underscore/
+        // esim _.each([1, 2, 3], function(num){ alert(num); });
+        for (i = 0;i<players.length;i+2) {
+            if (players[i].length > 0 && players[i+1].length > 0) {
+                switch (i) {
+                    case 0: // Create farm1 and tractor1
+                        break;
+                    case 2: // Create tractor2
+                        break;
+                    case 4: // Create farm2 and tractor1
+                        break;
+                    case 6: // Create tractor2
+                        break;
+                    case 8: // Create farm3 and tractor1
+                        break;
+                    case 10: // Create tractor2
+                        break;
+                    case 12: // Create farm4 and tractor1
+                        break;
+                    case 14: // Create tractor2
+                        break;
+                    case 16: // Create farm5 and tractor1
+                        break;
+                    case 18: // Create tractor2
+                        break;
+                    case 20: // Create farm6 and tractor1
+                        break;
+                    case 22: // Create tractor2
+                        break;
+                }
+
             }
-
         }
+
     }
+    
 }
