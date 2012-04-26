b3a306f16b71ed49fa3ff1c945a6811ad05ee4b7
Teemu <teemu@dyn-156-144.public.tut.fi>
2012-04-26 14:21:25 +0300

    fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,7 +14,7 @@ window.onload = function() {
 
           var widthToHeight = WIDTH / HEIGHT;
           var newWidth = window.innerWidth;
-          var newHeight = window.innerHeight - 54; // -54 header
+          var newHeight = window.innerHeight;
           var newWidthToHeight = newWidth / newHeight;
 
           if (newWidthToHeight > widthToHeight) {
@@ -24,15 +24,12 @@ window.onload = function() {
           }
 
           var marginLeft = (window.innerWidth - newWidth) / 2;
-          var fontSize = (newWidth / 1000) + "em";
 
           var $cr = $('#cr-stage');
           var $canvas = $cr.find('canvas');
 
           $canvas.css({ 'width': newWidth, 'height': newHeight, 'margin-left': marginLeft });
           $cr.css({ 'width': '100%', 'height': newHeight });
-          $('body').css({ 'font-size': fontSize });
-
   });
   
   $(window).trigger('resize');
