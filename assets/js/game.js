9cefcbb546a878557473976af8292bad2f1577a3
Teemu <teemu@dyn-156-144.public.tut.fi>
2012-04-26 14:57:40 +0300

    excellente

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,7 @@ window.onload = function() {
 
   // Load first scene
   Crafty.scene("Loading");
-  
-  /*
+
   $(window).on('resize orientationchange', function(event) {
 
           var widthToHeight = WIDTH / HEIGHT;
@@ -27,12 +26,15 @@ window.onload = function() {
           var marginLeft = (window.innerWidth - newWidth) / 2;
 
           var $cr = $('#cr-stage');
-          var $canvas = $cr.find('canvas');
-
-          $canvas.css({ 'width': newWidth, 'height': newHeight, 'margin-left': marginLeft });
-          $cr.css({ 'width': '100%', 'height': newHeight });
+          
+          // TODO count here right values 
+          $cr.css({ '-webkit-transform': 'translate3d(0, 0, 0) scale(.7)' });
+          
+          //var $canvas = $cr.find('canvas');
+          //$canvas.css({ 'width': newWidth, 'height': newHeight, 'margin-left': marginLeft });
+          //$cr.css({ 'width': '100%', 'height': newHeight });
   });
-  */
-  // $(window).trigger('resize');
+
+  $(window).trigger('resize');
 
 };
